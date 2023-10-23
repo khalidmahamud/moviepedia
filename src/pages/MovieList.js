4200e112cb146ee65a1ffced4ff6ddf3c4f512c1
Khalid <22-46601-1@student.aiub.edu>
2023-10-23 22:51:18 +0600

    added fetch api

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,28 @@
-export const MovieList = () => {
+import { useFetch } from "../hooks/useFetch";
+import { Card } from "../components";
+
+export const MovieList = ({ param }) => {
+    const API_KEY = process.env.REACT_APP_API_KEY;
+    const url = `https://api.themoviedb.org/3/${param}?api_key=${API_KEY}`;
+    const { data:movies } = useFetch(url);
+
     return (
         <main>
-            MovieList
+            <section className="max-w-7xl mx-auto py-7">
+                <div className="flex flex-wrap justify-center gap-5">
+
+                        {
+                            movies &&
+                            movies.map((movie) => (
+                                <Card 
+                                    key={movie.id}
+                                    movie={movie}
+                                />
+                            ))
+                        }
+
+                </div>
+            </section>
         </main>
     );
 };
--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -3,16 +3,16 @@ import { MovieList, MovieDetail, Search, PageNotFound } from '../pages';
 
 export const AllRoutes = () => {
     return (
-        <>
+        <div className='dark:bg-slate-800'>
             <Routes>
-                <Route path='' element={<MovieList />} />
-                <Route path='movies/popular' element={<MovieList />} />
-                <Route path='movies/top' element={<MovieList />} />
-                <Route path='movies/upcoming' element={<MovieList />} />
+                <Route path='' element={<MovieList param="movie/now_playing" />} />
+                <Route path='movies/popular' element={<MovieList param="movie/popular" />} />
+                <Route path='movies/top' element={<MovieList param="movie/top_rated" />} />
+                <Route path='movies/upcoming' element={<MovieList param="movie/upcoming" />} />
                 <Route path='movies/:id' element={<MovieDetail />} />
                 <Route path='search' element={<Search />} />
                 <Route path='*' element={<PageNotFound />} />
             </Routes>
-        </>
+        </div>
     );
 };
